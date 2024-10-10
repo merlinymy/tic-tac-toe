ad62e62ac86d08baca394daf8db2fe7fd6c32005
merlinymy <lordhorcrux@gmail.com>
2024-10-10 16:41:28 -0500

    improve game logic module

--- javascript.js
+++ javascript.js
@@ -9,7 +9,10 @@ const gameBoardModule = (function gameBoard() {
                    [null, null, null],
                    [null, null, null]];
 
-    return {board};
+    const checkState = () => {
+        console.table(board);
+    };
+    return {board, checkState};
 })();
 
 const playerFactory = function player() {
@@ -20,7 +23,7 @@ const playerFactory = function player() {
 
 const gameLogicModule = (function gameLogic() {
     const {player1, player2} = playerFactory();
-    const gameBoard = gameBoardModule;
+    const gameBoard = gameBoardModule.board;
     let curPlayer = player1;
 
     const switchPlayer = (cur) => {
@@ -29,16 +32,44 @@ const gameLogicModule = (function gameLogic() {
     };
 
     const playRound = (row, col) => {
+        // check if row, col num legal
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            console.log("illegal inputs, please provide row and col between 0 and 2");
+            return;
+        }
+
         console.log(`current player is ${curPlayer}`);
         console.log(`current player ${curPlayer} placed a piece at row: ${row},
                     col: ${col}`);
         console.log(`current board looks like:`);
-        console.table(gameBoard);
-        gameBoard.board[row][col] = curPlayer;
-        console.log(`current board looks like:`);
-        console.table(gameBoard);
-        switchPlayer(curPlayer);
-        console.log(`current player is ${curPlayer}`);
+        // console.table(gameBoard);
+        
+        // current player place a tokken with logic checking if
+        // space is available
+        let {isAvailable, errorMessage} = checkSpace(gameBoard, row, col);
+        
+        if (isAvailable) {
+            gameBoard[row][col] = curPlayer;
+            console.log("place piece success")
+            switchPlayer(curPlayer);
+
+            console.log(`current board looks like:`);
+            // console.table(gameBoard);
+            console.log(`current player is ${curPlayer}`);
+        } else {
+            console.log(errorMessage);
+        }
+        gameBoardModule.checkState();
+    }
+
+    const checkSpace = (board, row, col) => {
+        let isAvailable = true;
+        let errorMessage = "";
+        if (!!board[row][col]) {
+            isAvailable = false;
+            errorMessage = "can't place piece, a piece exist";
+        }
+        return {isAvailable, errorMessage};
     }
     
     return {playRound};
