266010ab06994c636fed868817a3cf9c022b6b3a
merlinymy <lordhorcrux@gmail.com>
2024-10-11 15:24:58 -0500

    refactor the project gomoku 15 x 15 board size

--- javascript.js
+++ javascript.js
@@ -1,19 +1,49 @@
+// project gomoku
+// Oct/11/2024
+
 // brainstorm
-// a factory for game board
-// a factory for player
-// a factory for game logic
+// 15 x 15 board
+// connect 5 to win (verticle, horizontal, diagonal)
 
 
 const gameBoardModule = (function gameBoard() {
-    const board = [[null, null, null],
-                   [null, null, null],
-                   [null, null, null]];
+    const board = [];
+    const cols = 15;
+    const rows = 15;
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < cols; j++) {
+            board[i].push(Unit());
+        }
+    }
+    
 
     const checkState = () => {
         console.table(board);
     };
-    return {board, checkState};
+
+    const printBoard = () => {
+        const prettyBoard = board.map((row) => row.map((cell) => cell.getState()));
+        console.log(prettyBoard);
+    }
+    return {board, checkState, printBoard};
 })();
+
+function Unit(){
+    // player piece
+    let state = null;
+
+    const setState = (player) => {
+        state = player;
+    }
+
+    const getState = () => {
+        return state;
+    }
+
+    return {setState, getState};
+}
 
 const playerFactory = function player() {
     const player1 = "x";
@@ -33,8 +63,12 @@ const gameLogicModule = (function gameLogic() {
 
     const playRound = (row, col) => {
         // check if row, col num legal
-        if (row < 0 || row > 2 || col < 0 || col > 2) {
+        if (row == null || col == null) {
             console.log("illegal inputs, please provide row and col between 0 and 2");
+            return;
+        }
+        if (row < 0 || row > 15 || col < 0 || col > 15) {
+            console.log("illegal inputs, please provide row and col between 0 and 15");
             return;
         }
 
@@ -42,30 +76,28 @@ const gameLogicModule = (function gameLogic() {
         console.log(`current player ${curPlayer} placed a piece at row: ${row},
                     col: ${col}`);
         console.log(`current board looks like:`);
-        // console.table(gameBoard);
+        // gameBoardModule.printBoard();
         
         // current player place a tokken with logic checking if
         // space is available
         let {isAvailable, errorMessage} = checkSpace(gameBoard, row, col);
         
         if (isAvailable) {
-            gameBoard[row][col] = curPlayer;
+            gameBoard[row][col].setState(curPlayer);
             console.log("place piece success")
             switchPlayer(curPlayer);
-
-            console.log(`current board looks like:`);
-            // console.table(gameBoard);
+            // gameBoardModule.printBoard();
             console.log(`current player is ${curPlayer}`);
         } else {
             console.log(errorMessage);
         }
-        gameBoardModule.checkState();
+        gameBoardModule.printBoard();
     }
 
     const checkSpace = (board, row, col) => {
         let isAvailable = true;
         let errorMessage = "";
-        if (!!board[row][col]) {
+        if (!!board[row][col].getState()) {
             isAvailable = false;
             errorMessage = "can't place piece, a piece exist";
         }
